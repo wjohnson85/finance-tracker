5e94a71c09895fe24a508880d1bc2999f18519ee
wjohnson85 <w.alexander.johnson@gmail.com>
2020-08-23 22:56:33 -0500

    added follow and searh users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'welcome#index'
-end
+get 'my_portfolio', to: 'users#my_portfolio'
+get 'stocks_search', to: 'stocks#search'
+get 'my_friends', to: 'users#my_friends'
+get 'search_friend', to: 'users#search'
+
+
+end 
